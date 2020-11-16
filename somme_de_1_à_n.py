615a300828bfa0a5cf11f95393d77786f13efbcc
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 20:07:51 +0100

    Add files via upload

---
+++ somme_de_1_à_n.py
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+n = int(input(“Entrez le nombre dont on veut la somme avec tous ses précédents : “))
+
+
+i = n
+calcul_correspondant = “La somme des chiffres de 1 à ”  + str(n) + “ est  “
+while (i > 1):
+  n += (i -1)
+  calcul_correspondant = calcul_correspondant + str(i)  + “+”
+  i += - 1
+print (calcul_correspondant + “1 soit : ”+str(n)+”.”)
+
+
+
+
+# ------------------------------------------------------------------- Résultat -----------------------------------------------------------------------------
